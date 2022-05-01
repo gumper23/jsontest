1bde1a3fa5436222e261fb17862500259be60d7a
Russ Smith <gumper@gumper.com>
2022-05-01 09:14:39 -0700

    Fixed some of the casing issues

--- api.go
+++ api.go
@@ -2,9 +2,9 @@ package main
 
 type API struct {
 	Port        string `json:"port"`
-	Clientcert  string `json:"clientcert"`
-	Clientkey   string `json:"clientkey"`
-	Servercert  string `json:"servercert"`
-	Serverkey   string `json:"serverkey"`
-	Releasemode string `json:"releasemode"`
+	ClientCert  string `json:"clientcert"`
+	ClientKey   string `json:"clientkey"`
+	ServerCert  string `json:"servercert"`
+	ServerKey   string `json:"serverkey"`
+	ReleaseMode string `json:"releasemode"`
 }
